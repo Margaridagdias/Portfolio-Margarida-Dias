44ac3784968fb979224a1dbf9a4ac0e7d5d562a3
Margarida <margarida97pt@gmail.com>
2021-07-12 10:54:28 +0100

    Updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,37 +5,55 @@ import "../components/Footer.css";
 const Footer = () => {
     
         return (
-            <footer className="footer">
-              <div className="footer__bg">
-                <div className="footer__container container grid">
-          <div>
-            <h1 className="footer__title">Margarida Dias</h1>
-            <span className="footer__subtitle">UX/UI Designer and Researcher</span>
-          </div>
-          <ul className="footer__links">
-           <li>
-              <a href="#home" className="footer__link">Home</a>
-            </li>
-          <li>
-              <a href="#about" className="footer__link">About Me</a>
-            </li>
-            <li>
-              <a href="#projects" className="footer__link">Projects</a>
-            </li>        
-          </ul>
-          <div className="footer__socials">
-            <a href="https://www.linkedin.com/in/margaridagdias/" className="footer__social">
-                  <i class="fab fa-linkedin-in"></i>
-            </a>
-             <a href="https://github.com/Margaridagdias" className="footer__social"> 
-                  <i class="fab fa-github"></i>
-            </a>
-          </div>
+          <footer className="footer">
+            <div className="footer__bg">
+              <div className="footer__container container grid">
+                <div>
+                  <h1 className="footer__title">Margarida Dias</h1>
+                  <span className="footer__subtitle">
+                    UX/UI Designer and Researcher
+                  </span>
                 </div>
-                <p className="footer__copy">&#169; by Margarida Dias</p>
+                <ul className="footer__links">
+                  <li>
+                    <a href="#home" className="footer__link">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#about" className="footer__link">
+                      About Me
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#education" className="footer__link">
+                      Education
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#projects" className="footer__link">
+                      Projects
+                    </a>
+                  </li>
+                </ul>
+                <div className="footer__socials">
+                  <a
+                    href="https://www.linkedin.com/in/margaridagdias/"
+                    className="footer__social"
+                  >
+                    <i class="fab fa-linkedin-in"></i>
+                  </a>
+                  <a
+                    href="https://github.com/Margaridagdias"
+                    className="footer__social"
+                  >
+                    <i class="fab fa-github"></i>
+                  </a>
+                </div>
               </div>
-            </footer>
-          
+              <p className="footer__copy">&#169; by Margarida Dias</p>
+            </div>
+          </footer>
         );
     }
 
